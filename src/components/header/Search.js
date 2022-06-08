23694b957f7c555e7b871e07273342996d8d690b
Farshad <krfarshad@gmail.com>
2022-06-08 16:42:36 +0430

    create result serach ajax

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -3,6 +3,8 @@ function Search() {
     const [search, setSearch] = useState(false);
     const [Products, setProducts] = useState([]);
     const [ProductsData, setProductsData] = useState([]);
+    const [searchProduct, setSearchProduct] = useState([]);
+
 
   
     const getProducts = async () => {
@@ -13,15 +15,29 @@ function Search() {
         setProductsData(data)
    
     }
+
+    const handle_search_product =(value) =>{
+            // Array.from(value).map(item=>{
+
+            // })
+            const results= Array.from(ProductsData).filter((item) => {   
+                const title = item.title.toLowerCase();
+                return title.indexOf(value) > -1;
+             });
+             setSearchProduct(results);
+    }
     const getSearchPost=(value)=>{
-        if(ProductsData){
+        if(!ProductsData.length){
             getProducts();
         }
-  
+        // handle search result
+        handle_search_product(value)
+
     }
 
     const handleSearchInput = (e) => {
         e.preventDefault();
+        e.stopPropagation();
         const inputValue = e.target.value;
         if (inputValue.length > 2) {
             setSearch(true);
@@ -34,9 +50,11 @@ function Search() {
         <>
             <input className="border border-gray-300 rounded-lg p-2 w-full md:w-96 bg-white shadow-sm shadow-gray-200 oultine-none
             focus:oultine-none  " type="search" placeholder="search ..." onChange={handleSearchInput} />
-            <div className={`search-wrapper shadow-sm shadow-gray-200 p-2 rounded-lg absolute left-8 -bottom-12
+            <div className={`search-wrapper shadow-sm shadow-gray-200 p-2 rounded-lg absolute left-8 top-14 z-20 max-h-96 overflow-hidden
              w-full md:w-96 bg-white opacity-0 hidden transition-all ease-linear ${search ? 'active-search' : ''} `}>
-                reseul
+                {searchProduct.length ? Array.from(searchProduct).map(item=>{
+                    return  <li className='py-4 text-sm list-none flex items-center border-b border-gray-200'><img className='w-10 h-10 object-contain rounded-lg overflow-auto mr-4' src={item.image} alt={item.title} /><a>{item.title}</a></li>  
+                }): `<p>چیزی یافت نشد.</p> ` }
             </div>
         </>
     )
--- src/components/header/menu/Menu.js
+++ src/components/header/menu/Menu.js
@@ -1,11 +1,11 @@
-import {   Routes, Route, Link } from "react-router-dom";
+import {   Routes, Route, Link, NavLink } from "react-router-dom";
 function Menu(){
  return(
       <nav>
         <ul>
             <li className="mt-1">
-                <Link className="mx-4 dark:text-white" to="/">Home</Link> 
-                <Link className="mx-4 dark:text-white" to="shop">Shop</Link>
+                <NavLink className="mx-4 dark:text-white" to="/">Home</NavLink> 
+                <NavLink className="mx-4 dark:text-white" to="shop">Shop</NavLink>
             </li>
         </ul>
      </nav>
